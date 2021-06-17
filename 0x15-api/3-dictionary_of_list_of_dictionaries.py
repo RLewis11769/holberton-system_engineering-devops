e71aed44d7bf084eb87ebff74d1ed335d68589bf
Rachel Lewis <RLewis11769@gmail.com>
2021-06-17 03:20:04 +0000

    Better comments and pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """
 Uses REST API to return jsonplaceholder employee productivity info
-Writes gathered info to .csv file
+Writes gathered info to .json file
 """
 
 import json
 import requests
 
-
-def export_all_to_json():
-    # Returns given info about given employee ID
-
-    allDict = {}
-    userDict = {}
-
-    users = requests.get('https://jsonplaceholder.typicode.com/users')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-
-    userJSON = users.json()
-    todoJSON = todos.json()
-
-    for user in userJSON:
-        userDict[user['id']] = user['username']
-    
-    for task in todoJSON:
-        if allDict.get(task['userId'], False) is False:
-            allDict[task['userId']] = []
-
-        taskDict = {}
-        taskDict["task"] = task['title']
-        taskDict["username"] = userDict[task['userId']]
-        taskDict["completed"] = task['completed']
-        # When each taskDict is built properly, add to final dict
-        allDict[task['userId']].append(taskDict)
-
-    # Write to empID.json file
-    with open('todo_all_employees.json', 'w') as jsonFile:
-        json.dump(allDict, jsonFile)
-
-# Only imports when called, but also passes first argument
 if __name__ == "__main__":
-    export_all_to_json()
+    def export_all_to_json():
+        """ Prints info about all employees to json file """
+
+        allDict = {}
+        userDict = {}
+
+        users = requests.get('https://jsonplaceholder.typicode.com/users')
+        todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+        userJSON = users.json()
+        todoJSON = todos.json()
+
+        # Sets up multiple lists for info about each user
+        for user in userJSON:
+            userDict[user['id']] = user['username']
+
+        # Format is: {empID: [{task key: task value}, {task key: task value}]}
+        for task in todoJSON:
+            # Creates new list for each user if valid userID
+            if allDict.get(task['userId'], False) is False:
+                allDict[task['userId']] = []
+            # Each user list should have format:
+            #   empID: name, True/False if completed, task title
+            taskDict = {}
+            # Dict format is userId as key, list info about tasks as value
+            taskDict["username"] = userDict[task['userId']]
+            taskDict["task"] = task['title']
+            taskDict["completed"] = task['completed']
+            # When each taskDict is built properly, add to final dict
+            allDict[task['userId']].append(taskDict)
+
+        # Writes to json file
+        with open('todo_all_employees.json', 'w') as jsonFile:
+            json.dump(allDict, jsonFile)
