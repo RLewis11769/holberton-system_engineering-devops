4273f82a48b2ab47037a6bed541af4595d633bec
Rachel Lewis <RLewis11769@gmail.com>
2021-06-17 03:37:03 +0000

    Better comments/explanation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" Uses REST API to return jsonplaceholder employee productivity info """
+"""
+Uses REST API to return jsonplaceholder employee productivity info
+Called with ./0 (employee ID number)
+First line: (employee name) is done with tasks(completed/total):
+Following lines: titles of completed tasks
+"""
 
 import requests
 import sys
 
 
 def get_employee_tasks(empID):
-    # Returns given info about given employee ID
+    """ Prints info about productivity of given employee ID """
 
     name = ''
     task_list = []
@@ -18,22 +23,25 @@ def get_employee_tasks(empID):
     todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                         .format(empID))
 
-    # Use json method to break api response into value at key "name"
+    # Use json method to break api response into useful info (at key name)
     userJSON = user.json()
     name = userJSON.get('name')
 
     todoJSON = todo.json()
+
+    # Gathers list of completed tasks and count
     for task in todoJSON:
         if task.get('completed') is True:
             completed += 1
             task_list.append(task.get('title'))
 
+    # Prints summary of given employee
     print("Employee {} is done with tasks({}/{}):".format(
         name, completed, len(todoJSON)))
+    # Prints completed tasks
     for task in task_list:
         print("\t {}".format(task))
 
 # Only imports when called, but also passes first argument
-# Called via "python3 0-gather_data_from_an_API.py (number)"
 if __name__ == "__main__":
     get_employee_tasks(sys.argv[1])
