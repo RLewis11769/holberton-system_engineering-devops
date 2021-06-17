a98353bdc3fbe2d50c40ed3248a5a2476362dc22
Rachel Lewis <RLewis11769@gmail.com>
2021-06-17 02:51:05 +0000

    Actually return conditional to add info about all tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,6 +2,7 @@
 """
 Uses REST API to return jsonplaceholder employee productivity info
 Writes gathered info to .csv file
+Called with ./1 (employee ID number)
 """
 
 import csv
@@ -10,7 +11,7 @@ import sys
 
 
 def export_to_csv(empID):
-    # Returns given info about given employee ID
+    """ Returns info about all tasks for given empID and status """
 
     name = ''
     task_list = []
@@ -24,23 +25,23 @@ def export_to_csv(empID):
     name = userJSON.get('username')
 
     todoJSON = todo.json()
+
     for task in todoJSON:
-        if task.get('completed') is True:
-            # Each line should have format:
-            #   empID, name, True/False if completed, task title
-            taskRow = []
-            taskRow.append(empID)
-            taskRow.append(name)
-            taskRow.append(task.get("completed"))
-            taskRow.append(task.get("title"))
-            # When each task line is built properly, add to row
-            task_list.append(taskRow)
+        # Each line should have format:
+        #   empID, name, True/False if completed, task title (in order)
+        taskRow = []
+        taskRow.append(empID)
+        taskRow.append(name)
+        taskRow.append(task.get("completed"))
+        taskRow.append(task.get("title"))
+        # When each task line is built properly, add to final list
+        task_list.append(taskRow)
 
     # Write to empID.csv file
     with open("{}.csv".format(empID), 'w') as csvFile:
         # Quote all fields aka "2","Shadow"
         csvwriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
-        # Write row from task_list to file
+        # Write all rows from task_list list to file in string format
         csvwriter.writerows(task_list)
 
 # Only imports when called, but also passes first argument
