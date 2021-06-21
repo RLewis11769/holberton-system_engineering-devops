59d4ba06fa8e581da5deac3a55c9b001fb210b04
Rachel Lewis <RLewis11769@gmail.com>
2021-06-21 21:43:39 +0000

    Add data to 102-zombie.c - thanks Caswell

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+        sleep(1);
+    return (0);
+}
+
+int main(void)
+{
+    pid_t pid;
+    int x;
+
+    for (x= 0; x < 5; x++)
+    {
+        pid = fork();
+        if (pid == 0)
+            exit(0);
+        printf("Zombie process created, PID: %d\n", pid);
+    }    
+    infinite_while();
+    return (0);
+}
+
